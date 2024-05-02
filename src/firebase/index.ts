f5d48ff5ea17d98179d5e908310a9b94ae4957ea
nesrucodex <155378885+nesrucodex@users.noreply.github.com>
2024-05-03 01:04:25 +0300

    Update index.ts

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,14 +4,6 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDN3DHzEgGG2CHOR-YNTy2oWJUe0vdL3bY",
-  authDomain: "coli-496e5.firebaseapp.com",
-  projectId: "coli-496e5",
-  storageBucket: "coli-496e5.appspot.com",
-  messagingSenderId: "93311192238",
-  appId: "1:93311192238:web:38913be61017be6d43713f"
-};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
